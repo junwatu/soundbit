db97d3f3a838cdc31e38174f45bb7f11285d4423
Equan Pr <equan.p@gmail.com>
2013-05-06 16:08:49 +0700

    using web audio api

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<header>
+    <h3>Have Some Fun</h3>
+</header>
+<div id="main">
+<form action="action.php">
+    <input type="text" name="url"/>
+    <input type="submit" value="Submit"/>
+</form>
+</div>
 <div id="player"></div>
 <script type="text/javascript" src="js/app.js"></script>
 </body>
