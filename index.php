5795e131328a235b165f70f0985b68d92cbf5e50
Equan Pr <eq@junwatu.com>
2013-05-08 14:09:35 +0700

    add canvas buttonto ui.

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Have Some Fun</title>
+    <title>SoundWig</title>
 
-<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div id="main">
-		<div id="info">
-			<img id="cover" class="cover">
-	    	<ul id="fileinfo">
-            	<li id="title"></li>
-                <li><audio autoplay="false" id="audioElement" controls></li>
-                <li><a href="http://soundcloud.com/" target="blank">
-                		<img src="images/logo_black.png"/>
-                	</a>
-            	</li>
-           	</ul>
-    	</div>
-		<canvas id="canvas"></canvas>
-	</div>
-	<footer>
-		<!--
-		<pre><a href="">Github</a> Copyright &copy; 2013 <a href="http://www.junwatu.com">JunWatu.Com</a></pre>
-		-->		
-	</footer>
-	<script type="text/javascript" src="http://connect.soundcloud.com/sdk.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
+<div id="main">
+    <div id="info">
+        <img id="cover" class="cover">
+        <ul id="fileinfo">
+            <li id="user" class="user"></li>
+            <li id="title"></li>
+        </ul>
+    </div>
+    <a href="http://soundcloud.com/" target="blank">
+        <img src="images/logo_black.png" id="soundcloud-logo"/>
+    </a>
+    <canvas id="playbutton" width="45" height="45"></canvas>
+    <canvas id="canvas"></canvas>
+</div>
+
+<footer>
+    <!--
+    <pre><a href="">Github</a> Copyright &copy; 2013 <a href="http://www.junwatu.com">JunWatu.Com</a></pre>
+	-->
+</footer>
+<script type="text/javascript" src="http://connect.soundcloud.com/sdk.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="js/button.js"></script>
 </body>
 </html>
